7fbcdf4ab2483b78d4612b54a0c2bbf4d472a90b
vitor <vitor-kiguchi@hotmail.com>
2025-12-03 19:33:36 -0300

    Day02 optimization

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,18 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-
+﻿
 var watch = System.Diagnostics.Stopwatch.StartNew();
 
 // var filename = "example.txt";
 var filename = "input";
 
-decimal Part1Answer = 0;
-decimal Part2Answer = 0;
+long Part1Answer = 0;
+long Part2Answer = 0;
 
 var text = File.ReadAllText(filename);
 
-foreach(var range in text.Split(',')) {
+long Part1(string ns) {
+    var len = ns.Length;
+    if(len % 2 == 1) {
+        return 0;
+    }
+    if(ns[..(len/2)] == ns[(len / 2)..]) {
+        return long.Parse(ns);
+    }
+    return 0;
+}
+
+long Part2(string ns) {
+    var len = ns.Length;
+
+    for(int i=1; i<=len/2; i++) {
+        if (len % i != 0) {
+            continue;
+        }
+        // var chunks = ns.Chunk(i);
+        // if (chunks.All(o => o.SequenceEqual(chunks.First()))) {
+        //     return long.Parse(ns);
+        // }
+        bool valid = true;
+        for(int j=i; j+i<=len; j+=i) {
+            if (ns[j..(j+i)] != ns[..i]) {
+                valid = false;
+                break;
+            }
+        }
+        if(valid) {
+            return long.Parse(ns);
+        }
+    }
+    return 0;
+}
+
+foreach(var range in text.Split(',').AsParallel().AsUnordered()) {
     var extremes = range.Split('-');
 
     var prefix = "";
@@ -26,30 +59,8 @@ foreach(var range in text.Split(',')) {
     var begin = int.Parse(extremes[0][nskip..]);
     var end = int.Parse(extremes[1][nskip..]);
 
-    foreach(var n in Enumerable.Range(begin, end-begin+1)) {
-        var ns = prefix + n.ToString();
-        var len = ns.Length;
-
-        for(int i=1; i<=len/2; i++)
-        {
-            var chunks = ns.Chunk(i);
-            if (chunks.All(o => new string(o) == new string(chunks.First()))) {
-                // Console.WriteLine($"{ns}: {System.Text.Json.JsonSerializer.Serialize(chunks)}");
-                Part2Answer += decimal.Parse(ns);
-                break;
-            }
-        }
-
-        // Part 1
-        if(len % 2 == 1) {
-            continue;
-        }
-        if(ns[..(len/2)] == ns[(len / 2)..])
-        {
-            // Console.WriteLine($"{prefix}, {n}");
-            Part1Answer += decimal.Parse(ns);
-        }
-    }
+    Part1Answer += ParallelEnumerable.Range(begin, end-begin+1).AsUnordered().Sum(n => Part1(prefix + n.ToString()));
+    Part2Answer += ParallelEnumerable.Range(begin, end-begin+1).AsUnordered().Sum(n => Part2(prefix + n.ToString()));
 }
 Console.WriteLine(Part1Answer);
 Console.WriteLine(Part2Answer);
